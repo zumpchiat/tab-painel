a0311241bbcc5e02021f90235251755251b95853
zumpchiat <zumpchiat@gmail.com>
2024-09-23 21:58:05 -0300

    page pedido head

--- src/app/(inside)/pedidos/page.tsx
+++ src/app/(inside)/pedidos/page.tsx
@@ -1,7 +1,66 @@
 "use client";
 
-import { Box } from "@mui/material";
+import { Refresh, Search } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 
 export default function Page() {
-  return <Box>Pagina de Pedidos</Box>;
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearchInput = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleSearchKey = () => {};
+
+  return (
+    <>
+      <Box sx={{ my: 3 }}>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography
+              component="h5"
+              variant="h5"
+              sx={{ color: "#555", mr: 2 }}
+            >
+              Pedidos
+            </Typography>
+            <Button
+              size="small"
+              sx={{ justifyContent: { xs: "flex-start", md: "center" } }}
+            >
+              <Refresh />
+              <Typography
+                component="div"
+                sx={{ color: "#555", display: { xs: "none", md: "block" } }}
+              >
+                Atualizar
+              </Typography>
+            </Button>
+          </Box>
+
+          <TextField
+            value={searchInput}
+            onChange={handleSearchInput}
+            onKeyUp={handleSearchKey}
+            placeholder="Pesquise um pedido"
+            variant="standard"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Search />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+      </Box>
+    </>
+  );
 }
