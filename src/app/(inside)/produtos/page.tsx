79b8b334486b2f4bd6284c98813859165419c3fe
zumpchiat <zumpchiat@gmail.com>
2024-10-04 10:52:09 -0300

    add base product page

--- src/app/(inside)/produtos/page.tsx
+++ src/app/(inside)/produtos/page.tsx
@@ -1,7 +1,53 @@
 "use client";
 
-import { Box } from "@mui/material";
+import {
+  Box,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { useState, useEffect } from "react";
 
 export default function Page() {
-  return <Box>Pagina de Produtos</Box>;
+  const [loading, setLoading] = useState(false);
+
+  const handleNewProduct = () => {};
+
+  return (
+    <>
+      <Box sx={{ my: 3, mt: 4, displayPrint: "none" }}>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography component="h5" variant="h5" sx={{ color: "#555", mr: 2 }}>
+            Produtos
+          </Typography>
+          <Button onClick={handleNewProduct}>Novo Produto</Button>
+        </Box>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell
+                sx={{ width: 50, display: { xs: "none", md: "table-cell" } }}
+              >
+                ID
+              </TableCell>
+              <TableCell>Imagem</TableCell>
+              <TableCell>Nome</TableCell>
+              <TableCell sx={{ display: { xs: "none", md: "table-cell" } }}>
+                Preço
+              </TableCell>
+              <TableCell sx={{ display: { xs: "none", md: "table-cell" } }}>
+                Categoria
+              </TableCell>
+              <TableCell sx={{ xs: 50, md: 130 }}>Ações</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody></TableBody>
+        </Table>
+      </Box>
+    </>
+  );
 }
